751e36c5c30ded2b9e2f1cc58d2b05662c29a6c0
rfi-buff <rf.ismet39@gmail.com>
2026-06-16 13:24:41 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -7,14 +7,22 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBj9t4Zemv86Psnzr0l0X6Q5DgmTxv-hkY",
+  authDomain: "rfi-portfolio.firebaseapp.com",
+  projectId: "rfi-portfolio",
+  storageBucket: "rfi-portfolio.firebasestorage.app",
+  messagingSenderId: "150811333694",
+  appId: "1:150811333694:web:d6bfce199e6d8bb462ad6f"
 };
+
+// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 
